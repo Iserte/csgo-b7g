885cfc7300097063408cf6036244316e6f37cf5c
Gustavo Iserte Bonfim <gustavo@dvdsp.com.br>
2019-12-16 09:13:14 -0300

    Updates

---
+++ precache-manifest.fcf50b576d05d332029532e125c559dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f129407bc34ffd19b2dbc66fdead728f",
+    "url": "/csgo-b7g/index.html"
+  },
+  {
+    "revision": "0fed2b2f3f8cc29066bc",
+    "url": "/csgo-b7g/static/js/2.35bd95a4.chunk.js"
+  },
+  {
+    "revision": "494c7c4d74968006d5a2",
+    "url": "/csgo-b7g/static/js/main.f781d6ae.chunk.js"
+  },
+  {
+    "revision": "ef17e6e0a5c2786eb8af",
+    "url": "/csgo-b7g/static/js/runtime-main.88e7452a.js"
+  },
+  {
+    "revision": "955622e6bb6eed0ee5f5a3fbdb6db0f8",
+    "url": "/csgo-b7g/static/media/background-forfun.955622e6.png"
+  },
+  {
+    "revision": "a390c1dd3254da29c6c3a1b57e2c6d11",
+    "url": "/csgo-b7g/static/media/background-lobby.a390c1dd.png"
+  },
+  {
+    "revision": "a390c1dd3254da29c6c3a1b57e2c6d11",
+    "url": "/csgo-b7g/static/media/background-login.a390c1dd.png"
+  }
+]);
